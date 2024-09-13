65c92f27401d671e7be32384b3da075bafbaabb6
nargiz <nargiz.nasirovar@gmail.com>
2024-09-13 19:38:42 +0200

    untracked files

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'NAME': 'clarusway',
         'USER': db_username,
         'PASSWORD': db_password,
-        'HOST': 'aws-capstone-rds.crqckckycg2i.us-east-1.rds.amazonaws.com',
+        'HOST': 'nargiz.crqckckycg2i.us-east-1.rds.amazonaws.com',
         'PORT': '3306',
     }
 }
@@ -156,7 +156,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'nargiz-capstone' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'capstone-django' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
