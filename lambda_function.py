065b5392173946e20eb47fa2e9d928ffb996dab6
Nargiz Nasirova <88719494+nargizraf@users.noreply.github.com>
2024-09-16 09:25:55 +0200

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -14,7 +14,7 @@ def lambda_handler(event, context):
         filename2 = filename1[-1]
         
         dynamo_db = boto3.resource('dynamodb')
-        dynamoTable = dynamo_db.Table('aws-capstone-dynamo')
+        dynamoTable = dynamo_db.Table('nargiz')
         
         dynamoTable.put_item(Item = {
             'id': filename2,
